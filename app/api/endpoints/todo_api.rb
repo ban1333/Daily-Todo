bcb9df9d3c8bc98bd05be0cbc86da16d2f139aec
David <fbjghuy@hotmail.com>
2023-08-31 15:49:55 -0400

    Create a get and put endpoint and start on the delete endpoint

--- app/api/endpoints/todo_api.rb
+++ app/api/endpoints/todo_api.rb
@@ -2,8 +2,12 @@ require "grape"
 
 module Endpoints
   class TodoApi < Grape::API
+    resource Todo do
+      desc 'Gets all todos'
+      get :todo do
+        Todo.all
+      end
 
-    # resource Todo do
       desc 'Create a todo'
       params do
         requires :name, type: String, desc: 'the task you wish to complete'
@@ -15,6 +19,18 @@ module Endpoints
                        info: params[:info]
                      })
       end
-    # end
+      desc 'Update a todo'
+      params do
+        requires :id, type: String, desc: 'the id of the todo'
+        optional :name, type: String, desc: 'the task you wish to complete'
+        optional :info, type: String, desc: 'more info about the task'
+      end
+      put ':id' do
+        Todo.find(params[:todo][:id]).update!({
+                                                name: params[:todo][:name],
+                                                info: params[:todo][:info]
+                                              })
+      end
+    end
   end
 end
--- spec/api/endpoints/todoapi_spec.rb
+++ spec/api/endpoints/todoapi_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe 'todo' do
-  let(:res) { JSON.parse(response.body)['response'] }
+  let(:res) { JSON.parse(response.body)[0] }
 
   context 'todo endpoints' do
     context 'post' do
-      let(:request) { post '/todo', :params => { name: name, info: info } }
+      let(:request) { post '/Todo/todo', :params => { name: name, info: info } }
       let(:name) { 'clean' }
       let(:info) { 'room' }
       let(:name_2) {'ooga'}
@@ -27,8 +27,8 @@ describe 'todo' do
 
       it 'creates multiple todos' do
         request
-        post '/todo', :params => { name: name_2, info: info_2 }
-        post '/todo', :params => { name: name_3, info: info_3 }
+        post '/Todo/todo', :params => { name: name_2, info: info_2 }
+        post '/Todo/todo', :params => { name: name_3, info: info_3 }
         expect(Todo.count).to eq 3
         expect(Todo.first.name).to eq name
         expect(Todo.first.info).to eq info
@@ -36,6 +36,90 @@ describe 'todo' do
         expect(Todo.second.info).to eq info_2
         expect(Todo.third.name).to eq name_3
         expect(Todo.third.info).to eq info_3
+      end
+    end
+
+    context 'put' do
+      let(:response) { put '/Todo/:id', :params => { todo: { id: Todo.first.id, name: name, info: info }}}
+      let(:name) { 'this is new' }
+      let(:info) { 'so is this' }
+      let(:old_name) { 'this is old' }
+      let(:old_info) { 'this old too' }
+
+      before do
+        Todo.create({name: old_name, info: old_info})
+      end
+
+      it 'updates the values of a todo' do
+        expect(Todo.first.name).to eq old_name
+        expect(Todo.first.info).to eq old_info
+        response
+        expect(Todo.first.name).to eq name
+        expect(Todo.first.info).to eq info
+      end
+    end
+
+    context 'get' do
+      let(:request) { get '/Todo/todo' }
+
+      context 'when there are no todos' do
+        it 'does not return a todo' do
+          request
+          expect(res).to eq nil
+        end
+      end
+
+      context 'when there is one todo' do
+        let(:name) { 'ooga' }
+        let(:info) { 'booga' }
+
+        before do
+          Todo.create!({
+                         name: name,
+                         info: info
+                       })
+        end
+
+        it 'gets the todo' do
+          request
+          expect(res['name']).to eq name
+          expect(res['info']).to eq info
+        end
+      end
+
+      context 'when there are multiple todos' do
+        let(:name) { 'ahh' }
+        let(:info) { 'ohh' }
+        let!(:todo_1) { Todo.create!({ name: name, info: info })}
+        let!(:todo_2) { Todo.create!({ name: name, info: info })}
+        let!(:todo_3) { Todo.create!({ name: name, info: info })}
+
+        it 'gets the todo' do
+          request
+          expect(JSON.parse(response.body).count).to eq 3
+          expect(res['name']).to eq name
+          expect(res['info']).to eq info
+        end
+      end
+    end
+
+    context 'delete' do
+      context 'when there is one todo' do
+        let(:request) { delete '/Todo/:id', :params => { todo: { id: Todo.first.id } } }
+
+        before do
+          Todo.create!({ name: 'ahh', info: 'ohh' })
+        end
+
+        it 'deletes the todo' do
+          expect(Todo.count).to be 1
+          request
+          expect(Todo.count).to be 0
+        end
+      end
+
+      context 'when there are multiple todos' do
+
       end
     end
   end
