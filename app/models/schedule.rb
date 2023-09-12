b62f2e1ffd4c6125cc659a4da886dfab97015161
David <fbjghuy@hotmail.com>
2023-09-12 18:59:03 -0400

    Add a scope on the schedule to get all schedules for today

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,8 @@
 class Schedule < ApplicationRecord
   belongs_to :todo
+
+  scope :all_today, -> do
+    thingy = DateTime.now.strftime("%A").downcase.to_sym
+    where(:"#{DateTime.now.strftime("%A").downcase}" => true)
+  end
 end
