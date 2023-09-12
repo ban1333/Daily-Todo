f9c0381a20dbaf0be60d8dc67ab1416a8f8dac30
David <fbjghuy@hotmail.com>
2023-09-12 15:59:35 -0400

    Add a post, put and delete endpoint to the completed model

--- app/api/endpoints/completed_api.rb
+++ app/api/endpoints/completed_api.rb
@@ -14,7 +14,38 @@ module Endpoints
         requires :id, type: String, desc: 'the id of the todo you wish to get'
       end
       get ':id' do
-        Completed.where(todo_id: params[:id])
+        Completed.where(todo_id: params[:completed][:id])
+      end
+
+      desc 'Create a Completed'
+      params do
+        requires :todo_id, type: String, desc: 'the id of the todo'
+        requires :date_completed, type: DateTime, desc: 'the date/time the todo was finished'
+      end
+      post :completed do
+        Completed.create!({
+                       date_completed: params[:date_completed],
+                       todo_id: params[:todo_id]
+                     })
+      end
+
+      desc 'Update a completed'
+      params do
+        requires :id, type: String, desc: 'the id of the completed'
+        requires :date_completed, type: DateTime, desc: 'the date/time the todo was finished'
+      end
+      put ':id' do
+        Completed.find(params[:completed][:id]).update!({
+                                                date_completed: params[:date_completed].change(:usec => 0)
+                                              })
+      end
+
+      desc 'Delete a completed'
+      params do
+        requires :id, type: String, desc: 'the id of the completed'
+      end
+      delete ':id' do
+        Completed.find(params[:completed][:id]).delete
       end
     end
   end
--- spec/api/endpoints/completeds_api_spec.rb
+++ spec/api/endpoints/completeds_api_spec.rb
@@ -118,4 +118,98 @@ describe 'CompletedsApi' do
       end
     end
   end
+
+  context 'when the post endpoint is hit' do
+    let(:request) { post '/Completed/completed', :params => desired_fields }
+    let(:todo) { Todo.create!({ name: 'ahh', info: 'ohh' }) }
+    let(:utc) { DateTime.now }
+    let(:est) { utc.in_time_zone('Eastern Time (US & Canada)') }
+    let(:desired_fields) { { date_completed: utc, todo_id: todo.id  } }
+
+    context 'with no completeds in the database' do
+      it 'creates a completed' do
+        expect(Completed.count).to eq 0
+        request
+        expect(Completed.count).to eq 1
+      end
+
+      it 'has the desired fields' do
+        request
+        expect(Completed.first.todo_id).to eq desired_fields[:todo_id]
+        expect(Completed.first.date_completed).to eq utc.to_fs(:iso8601)
+      end
+    end
+
+    context 'with one completed in the database' do
+      let!(:completed) { Completed.create!({ todo_id: todo.id, date_completed: DateTime.now }) }
+
+      it 'creates a completed' do
+        expect(Completed.count).to eq 1
+        request
+        expect(Completed.count).to eq 2
+      end
+
+      it 'has the desired fields' do
+        request
+        expect(Completed.last.todo_id).to eq desired_fields[:todo_id]
+        expect(Completed.last.date_completed).to eq utc.to_fs(:iso8601)
+      end
+    end
+
+    context 'with three completed in the database' do
+      let!(:completed) { Completed.create!({ todo_id: todo.id, date_completed: DateTime.now }) }
+      let!(:completed) { Completed.create!({ todo_id: todo.id, date_completed: DateTime.now }) }
+      let!(:completed) { Completed.create!({ todo_id: todo.id, date_completed: DateTime.now }) }
+
+      it 'creates a completed' do
+        expect(Completed.count).to eq 1
+        request
+        expect(Completed.count).to eq 2
+      end
+
+      it 'has the desired fields' do
+        request
+        expect(Completed.last.todo_id).to eq desired_fields[:todo_id]
+        expect(Completed.last.date_completed).to eq utc.to_fs(:iso8601)
+      end
+    end
+  end
+
+  context 'when the put endpoint is hit' do
+    #let(:request) { put '/Completed/:id', :params => { completed: { id: completed.id, date_completed: updated_completed.date_completed } } }
+    let(:request) { put '/Completed/:id', :params => { completed: { id: completed.id}, date_completed: updated_completed.date_completed } }
+
+    context 'when there is one completed in the database' do
+      let(:todo) { Todo.create!({ name: 'ooh', info: 'ahh' }) }
+      let!(:completed) { Completed.create!({ todo_id: todo.id, date_completed: DateTime.now }) }
+      let(:updated_completed) { Completed.new({ todo_id: todo.id, date_completed: completed.date_completed - 3.days }) }
+
+      it 'only has one completed before and after' do
+        expect(Completed.count).to be 1
+        request
+        expect(Completed.count).to be 1
+      end
+
+      it 'updates the completed to the desired fields' do
+        request
+        expect(Completed.first.todo_id).to eq updated_completed.todo_id
+        expect(Completed.first.date_completed).to eq updated_completed.date_completed.change(:usec => 0)
+      end
+    end
+  end
+
+  context 'when the delete endpoint is hit' do
+    let(:request) { delete '/Completed/:id', :params => { completed: { id: completed.id }} }
+
+    context 'when there is one Completed in the database' do
+      let!(:completed) { Completed.create({ todo_id: todo.id, date_completed: DateTime.now }) }
+      let(:todo) { Todo.create!({ name: 'ohh', info: 'ahh' }) }
+
+      it 'deletes the completed' do
+        expect(Completed.count).to be 1
+        request
+        expect(Completed.count).to be 0
+      end
+    end
+  end
 end
