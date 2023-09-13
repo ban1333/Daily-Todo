0d8f3671d7434363cbae1d3d6ccb188737537cdb
David <fbjghuy@hotmail.com>
2023-09-13 13:57:52 -0400

    Add a scope to get all of the todos for the current day

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,9 @@
 class Todo < ApplicationRecord
   has_one :schedule
+
+  scope :todos_for_today, -> do
+    Todo.all
+        .joins(:schedule)
+      .where(schedules: {:"#{DateTime.now.strftime("%A").downcase}" => true})
+  end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'scopes' do
+    context 'is_today' do
+      let(:todo_1) { Todo.create!({ name: 'one', info: 'ahh' }) }
+      let(:todo_2) { Todo.create!({ name: 'two', info: 'ahh' }) }
+      let(:todo_3) { Todo.create!({ name: 'three', info: 'ahh' }) }
+      let(:todo_4) { Todo.create!({ name: 'four', info: 'ahh' }) }
+      let(:todo_5) { Todo.create!({ name: 'five', info: 'ahh' }) }
+      let(:todo_6) { Todo.create!({ name: 'six', info: 'ahh' }) }
+      let!(:schedule_1) { Schedule.create!({ todo_id: todo_1.id, sunday: 1, monday: 1, tuesday: 1, wednesday: 1, thursday: 1, friday: 1, saturday: 1 }) }
+      let!(:schedule_2) { Schedule.create!({ todo_id: todo_2.id, sunday: 1, monday: 1, tuesday: 1, wednesday: 1, thursday: 1, friday: 1, saturday: 1 }) }
+      let!(:schedule_3) { Schedule.create!({ todo_id: todo_3.id, sunday: 1, monday: 1, tuesday: 1, wednesday: 1, thursday: 1, friday: 1, saturday: 1 }) }
+      let!(:schedule_not_today_1) { Schedule.create!({ todo_id: todo_4.id, sunday: 0, monday: 0, tuesday: 0, wednesday: 0, thursday: 0, friday: 0, saturday: 0 }) }
+      let!(:schedule_not_today_2) { Schedule.create!({ todo_id: todo_5.id, sunday: 0, monday: 0, tuesday: 0, wednesday: 0, thursday: 0, friday: 0, saturday: 0 }) }
+      let!(:schedule_not_today_3) { Schedule.create!({ todo_id: todo_6.id, sunday: 0, monday: 0, tuesday: 0, wednesday: 0, thursday: 0, friday: 0, saturday: 0 }) }
+
+      it 'returns a schedule that is today' do
+        expect(Todo.todos_for_today.count).to be 3
+        expect(Schedule.first.todo_id).to eq schedule_1.todo_id
+        expect(Schedule.second.todo_id).to eq schedule_2.todo_id
+        expect(Schedule.third.todo_id).to eq schedule_3.todo_id
+      end
+    end
+  end
 end
