249375f4bdcbb15dc6bb374760b1c1dfbcf2c171
David <fbjghuy@hotmail.com>
2023-09-13 13:38:17 -0400

    Make todos and schedules a 1 to 1 relationship

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,8 +1,3 @@
 class Schedule < ApplicationRecord
-  belongs_to :todo
-
-  scope :all_today, -> do
-    thingy = DateTime.now.strftime("%A").downcase.to_sym
-    where(:"#{DateTime.now.strftime("%A").downcase}" => true)
-  end
+  belongs_to :todo, optional: true
 end
